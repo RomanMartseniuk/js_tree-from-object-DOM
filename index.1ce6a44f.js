2cccfc912bab3ab464e53d42533225f9f8e67e0b
Roman Martseniuk <roman.martseniuk.developer@gmail.com>
2024-09-16 22:41:20 +0300

    make build

---
+++ index.1ce6a44f.js
@@ -0,0 +1,2 @@
+!function e(t,n){for(var r in n){var a=document.createElement("li");if(a.textContent=r,"object"==typeof n[r]&&Object.keys(n[r]).length>0){var l=document.createElement("ul");e(l,n[r]),a.appendChild(l)}t.appendChild(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.1ce6a44f.js.map
